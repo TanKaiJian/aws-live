2e84a2feb3043ca46608ab6cb4c4459eac5c3407
TAN KAI JIAN <125535803+TanKaiJian@users.noreply.github.com>
2024-09-02 15:14:49 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.ci0ul6uglacd.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "jayden-employee"
+customregion = "us-east-1"
+
